593c96c86be75933a6834f2614782d8727e8cdcf
Guilherme <30673354+djnakudo@users.noreply.github.com>
2018-02-01 23:07:31 -0200

    Update Vote schema to have less docs
    In this approach, I just changed the vote schema to have the points as Number, so one can update the votes for specific OS by just increment this file.I will pull request the other changes to structure the solution based on this schema version.

--- models/Vote.js
+++ models/Vote.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
+
 const Schema = mongoose.Schema;
 
 const VoteSchema = new Schema({
-  os: {
-    type: String,
-    required: true
-  },
-  points: {
-    type: String,
-    required: true
-  }
-});
-
+    os:{
+        type:String,
+        required:true
+    },
+  //points as Number to be an incremental field
+    points :{
+        type:Number,
+        default:0
+    }
+})
 // Create collection and add schema
-const Vote = mongoose.model('Vote', VoteSchema);
-
+const Vote = mongoose.model('vote',VoteSchema);
 module.exports = Vote;
